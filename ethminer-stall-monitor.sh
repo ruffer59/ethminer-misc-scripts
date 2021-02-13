5f8afce36d0b72c955b91515a9efd65a684ed738
ruffer59 <matrox.millennium@gmail.com>
2021-02-13 16:11:11 +0900

    edit conditions as ethminer-stall-monitor.sh

--- ethminer-stall-monitor.sh
+++ ethminer-stall-monitor.sh
@@ -6,7 +6,7 @@ while [ $FIELD_NUM -ne 0 ]
 do
 	WORD=`tail $LOG_FILE -n 100 | grep " m " | head -n1 | awk -F "-" '{ print $2 }' | awk -F "," '{ print $(expr '$FIELD_NUM') }' |awk '{ print $1 }'`
 	
-	if [ $(tail $LOG_FILE -n 100 | awk -F $WORD '{ print $2 }' | awk -F "," '{ print $1 }' | grep -v '^\s*$' | awk '{ print $1 }' | grep 0.00 | head -n1) ]; then
+	if [ $(tail $LOG_FILE -n 100 | awk -F $WORD '{ print $2 }' | awk -F "," '{ print $1 }' | grep -v '^\s*$' | awk '{ print $1 }' | grep m0.00 | head -n1) ]; then
 		/root/ethminer-kick-screen.sh
 	else
 		:
