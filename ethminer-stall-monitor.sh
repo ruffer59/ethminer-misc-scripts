7ff25cbf2d0a227726392146e3a7d5445bb812fe
ruffer59 <matrox.millennium@gmail.com>
2021-06-08 23:52:14 +0900

    fix as sed command, eval if statement

--- ethminer-stall-monitor.sh
+++ ethminer-stall-monitor.sh
@@ -5,8 +5,9 @@ FIELD_NUM=`tail $LOG_FILE -n 100 | grep " m " | head -n1 | awk -F "-" '{ print $
 while [ $FIELD_NUM -ne 0 ]
 do
 	WORD=`tail $LOG_FILE -n 100 | grep " m " | head -n1 | awk -F "-" '{ print $2 }' | awk -F "," '{ print $(expr '$FIELD_NUM') }' |awk '{ print $1 }'`
-	
-	if [ $(tail $LOG_FILE -n 100 | awk -F $WORD '{ print $2 }' | awk -F "," '{ print $1 }' | grep -v '^\s*$' | awk '{ print $1 }' | grep m0.00 | head -n1) ]; then
+        FLAG=`tail $LOG_FILE -n 100 | awk -F $WORD '{ print $2 }' | awk -F "," '{ print $1 }' | grep -v '^\s*$' | awk '{ print $1 }' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g" | grep ^"0.00" | head -n1`
+
+	if [ $FLAG ]; then
 		/root/ethminer-kick-screen.sh
 	else
 		:
